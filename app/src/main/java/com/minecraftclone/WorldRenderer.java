c618d5d8de1ac574ab8678ca59f8a7d59f2d58ad
vangohner <52749520+vangohner@users.noreply.github.com>
2025-09-13 13:08:28 -0400

    Enable backface culling

--- app/src/main/java/com/minecraftclone/WorldRenderer.java
+++ app/src/main/java/com/minecraftclone/WorldRenderer.java
@@ -109,6 +109,9 @@ public class WorldRenderer {
         GL.createCapabilities();
 
         glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+        glFrontFace(GL_CCW);
         glClearColor(0.53f, 0.81f, 1f, 0f);
 
         updateProjection();
